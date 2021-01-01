aef8c832b2a598b824718a18db9bfc0fed0af126
Nitish Kumar <niralanitrogensir@gmail.com>
2021-01-01 23:55:59 +0530

    This is the first commit . In This commit I am define all view and import class.

--- app/src/main/java/com/example/basiccalculator/MainActivity.java
+++ app/src/main/java/com/example/basiccalculator/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.basiccalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText firstnumber,secondNumber;
+    TextView textView;
+    Button add,substract,multiply,divide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
